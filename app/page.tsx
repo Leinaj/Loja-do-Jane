7f6178533d9ccabbd5df28ae0aec54142d0577fb
Leinaj <janielbs@gmail.com>
2025-11-02 22:28:21 -0300

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Loja da Jane</h1>
-      <p>Se você está vendo isso, deu certo! 🎉</p>
-    </main>
-  );
-}
