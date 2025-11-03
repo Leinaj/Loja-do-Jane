f09f0ddfb22d619e003bd49124384cdfe0fe2ba5
Leinaj <janielbs@gmail.com>
2025-11-03 16:55:52 -0300

    Configure remote image patterns in next.config.js
    Added remote image patterns for external sources.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'placehold.co' },        // placeholder simples
+      { protocol: 'https', hostname: 'images.unsplash.com' }, // se quiser usar unsplash
+      { protocol: 'https', hostname: 'i.imgur.com' },         // imgur
+      { protocol: 'https', hostname: 'cdn.shopify.com' }      // opcional
+    ],
+  },
+};
+
 module.exports = nextConfig;
