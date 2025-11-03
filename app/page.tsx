af257f85b7dc4f354d068ac9f6deafcfe04ea7b0
Leinaj <janielbs@gmail.com>
2025-11-02 22:03:17 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Loja da Jane</h1>
+      <p>Se você está vendo isso, deu certo! 🎉</p>
+    </main>
+  );
+}
