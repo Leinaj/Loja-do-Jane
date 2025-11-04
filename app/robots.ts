9965d01dc56af71413be55a9b00a3be3f1fcf371
Leinaj <janielbs@gmail.com>
2025-11-04 20:03:26 -0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,9 @@
-
+// app/robots.ts
+import type { MetadataRoute } from "next";
+export default function robots(): MetadataRoute.Robots {
+  const base = "https://loja-do-jane.vercel.app";
+  return {
+    rules: [{ userAgent: "*", allow: "/" }],
+    sitemap: `${base}/sitemap.xml`,
+  };
+}
