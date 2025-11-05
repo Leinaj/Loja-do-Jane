066cc273098e0a2edb658aab0f61bd9978889390
Leinaj <janielbs@gmail.com>
2025-11-04 22:13:39 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
-module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: { sans: ["var(--font-inter)", "system-ui", "sans-serif"] },
-    },
-  },
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: { extend: {} },
   plugins: [],
+  darkMode: "class"
 };
+
+export default config;
