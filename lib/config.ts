85f480cc5f9267f6f3ee7a09c4512bd4c4b8d63d
Leinaj <janielbs@gmail.com>
2025-11-05 18:57:18 -0300

    Criar lib/config.ts

---
+++ lib/config.ts
@@ -0,0 +1,7 @@
+// lib/config.ts
+export const STORE = {
+  name: "Loja da Jane",
+  whatsappIntl: "+5544988606483", // com DDI
+  whatsappPretty: "+55 (44) 98860-6483",
+  pixKey: "44988606483",
+};
