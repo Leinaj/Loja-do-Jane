b880a4b7925d98c7a4a62dfce7afd8dd71e2b1b1
Leinaj <janielbs@gmail.com>
2025-11-06 16:50:58 -0300

    Atualizar o tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
+import type { Config } from "tailwindcss";
+const config: Config = {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: { extend: {} },
-  plugins: [],
+  plugins: []
 };
+export default config;
