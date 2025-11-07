d62df6c98758b558c577b938b46b2c7c0a741eeb
Leinaj <janielbs@gmail.com>
2025-11-07 15:15:14 -0300

    Delete lib directory

--- lib/site.ts
+++
@@ -1,7 +0,0 @@
-// lib/site.ts
-export const site = {
-  name: "Loja do Jane",
-  description: "Produtos selecionados com preço justo.",
-  url: process.env.NEXT_PUBLIC_SITE_URL ?? "https://loja-do-jane.vercel.app",
-  whatsapp: "5544988606483", // E.164 sem '+'
-};
