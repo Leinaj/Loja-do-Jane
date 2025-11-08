e500a2ff0d91e59bfa0df5a0bb16a37a39f19445
Leinaj <janielbs@gmail.com>
2025-11-08 19:39:37 -0300

    Atualizar o providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,13 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { CartProvider } from '../lib/cart';
-import { ToastContainer } from '../components/ui/toast';
+import { CartProvider } from '@/lib/cart';
+import { ToastContainer } from '@/components/ui/toast';
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <CartProvider>
       {children}
-      {/* Container global para toasts */}
       <ToastContainer />
     </CartProvider>
   );
