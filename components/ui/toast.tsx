0babebc0349a2f4221ce3dff2afa14e2625644da
Leinaj <janielbs@gmail.com>
2025-11-09 17:09:13 -0300

    Atualizar o toast.tsx

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useCart } from '@/components/cart/context';
+import { useCart } from '@/app/components/cart/context';
 
 type Toast = { id: number; text: string };
 
