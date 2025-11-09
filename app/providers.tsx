908434d66ab06141b9ff179af3d3a5b338f4bc7d
Leinaj <janielbs@gmail.com>
2025-11-09 17:20:00 -0300

    Atualizar o providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import React from 'react';
-
-// 👇 note o ".." (sobe da pasta /app para a raiz)
 import { CartProvider } from '../components/cart/context';
 import ToastBridge from '../components/ui/toast';
 
