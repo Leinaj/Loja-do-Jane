2b3b9de40b629920f6c0e30ba281c7ddaa06e179
Leinaj <janielbs@gmail.com>
2025-11-09 08:49:08 -0300

    Atualizar o RootProviders.tsx

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -2,13 +2,11 @@
 
 import React from 'react';
 import { CartProvider } from '@/lib/cart';
-import ToastBridge from '@/components/ui/toast'; // se seu toast está em outro caminho, ajuste
 
-export default function RootProviders({ children }: { children: React.ReactNode }) {
-  return (
-    <CartProvider>
-      {children}
-      <ToastBridge />
-    </CartProvider>
-  );
+export default function RootProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <CartProvider>{children}</CartProvider>;
 }
