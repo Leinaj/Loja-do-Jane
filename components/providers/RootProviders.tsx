79faa6b3ae1f34819553db71eb38a29be5d56c35
Leinaj <janielbs@gmail.com>
2025-11-08 22:34:41 -0300

    Atualizar o RootProviders.tsx

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -1,14 +1,9 @@
 'use client';
 
 import React from 'react';
-import { ToastProvider, ToastBridge } from '@/components/ui/toast';
+import { ToastProvider } from '@/components/ui/toast';
 
 export default function RootProviders({ children }: { children: React.ReactNode }) {
-  return (
-    <ToastProvider>
-      {/* Compatibilidade com projetos que esperam esse componente */}
-      <ToastBridge />
-      {children}
-    </ToastProvider>
-  );
+  // Aqui você pode colocar outros providers (Theme, Query, etc.) no futuro.
+  return <ToastProvider>{children}</ToastProvider>;
 }
