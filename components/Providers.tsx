e46a248dd17547d029a076f467846692c9baba77
Leinaj <janielbs@gmail.com>
2025-11-10 18:30:51 -0300

    Atualizar o Providers.tsx

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,6 +1,7 @@
+// components/Providers.tsx
 'use client';
 
-import { CartProvider } from '@/lib/cart';
+import { CartProvider } from '@/components/cart/context';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return <CartProvider>{children}</CartProvider>;
