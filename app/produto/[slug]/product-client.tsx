2698b9be453f1c0de7e7f1eef9997a40fbe49d4d
Leinaj <janielbs@gmail.com>
2025-11-13 17:53:44 -0300

    Atualizar o product-client.tsx

--- app/produto/[slug]/product-client.tsx
+++ app/produto/[slug]/product-client.tsx
@@ -1,4 +1,3 @@
-{/* TOAST */}
 {showToast && (
   <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-[9999] w-[90%] max-w-md pointer-events-none">
     <div className="bg-zinc-900 border border-zinc-700 rounded-2xl px-4 py-3 shadow-xl flex gap-3">
